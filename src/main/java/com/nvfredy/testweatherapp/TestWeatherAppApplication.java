df813523ff1ed888cf9e56598fe4d71e7be14193
Andrey Fyodorov <afedorov@rdlab.info>
2022-02-21 15:13:22 +0500

    Initial commit

---
+++ src/main/java/com/nvfredy/testweatherapp/TestWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.nvfredy.testweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvfredy/testweatherapp/TestWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvfredy.testweatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
